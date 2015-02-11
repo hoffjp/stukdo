2b7d0d607052d52b90850df452b7040d010d73db
Jacob Hoffmann <hoff.jp12@gmail.com>
2015-02-10 23:23:34 -0500

    added states and organized the index page showing what state they are in

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,7 +5,9 @@ class TasksController < ApplicationController
   # GET /tasks
   # GET /tasks.json
   def index
-    @tasks = current_user.tasks
+    @to_do = current_user.tasks.where(state: "To Do")
+    @doing = current_user.tasks.where(state: "Doing")
+    @done = current_user.tasks.where(state: "Done")
   end
 
   # GET /tasks/1
@@ -70,6 +72,6 @@ class TasksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def task_params
-      params.require(:task).permit(:content)
+      params.require(:task).permit(:content, :state)
     end
 end
