812a96b247439049934e1e36d1f311e3e704aa70
Jacob Hoffmann <hoff.jp12@gmail.com>
2015-02-25 23:18:18 -0500

    added auto_html gem and added abiltiy to see show videos and links in tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150211035727) do
+ActiveRecord::Schema.define(version: 20150226040505) do
 
   create_table "tasks", force: :cascade do |t|
     t.text     "content"
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
     t.integer  "user_id"
-    t.string   "state",      default: "To Do"
+    t.string   "state",        default: "To Do"
+    t.text     "content_html"
   end
 
   create_table "users", force: :cascade do |t|
